ac1e2cd906a3363c80ce3527825a90633e14f0c0
linux_china <linux_china@hotmail.com>
2014-05-20 11:45:18 +0800

    hbase native client test

---
+++ src/test/java/org/mvnsearch/hbase/HBaseNativeClientTest.java
@@ -0,0 +1,46 @@
+package org.mvnsearch.hbase;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * hbase native client test
+ *
+ * @author linux_china
+ */
+public class HBaseNativeClientTest {
+    private static HTable userTable;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        Configuration conf = HBaseConfiguration.create();
+        conf.set("hbase.zookeeper.quorum", "192.168.33.33");
+        userTable = new HTable(conf, "user");
+    }
+
+    @Test
+    public void testSpike() throws Exception {
+        Result result = userTable.get(new Get(Bytes.toBytes("1")));
+        List<KeyValue> column = result.getColumn(Bytes.toBytes("cf"), Bytes.toBytes("nick"));
+        byte[] value = column.get(0).getValue();
+        System.out.println(new String(value));
+        userTable.close();
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        if (userTable != null) {
+            userTable.close();
+        }
+    }
+}
